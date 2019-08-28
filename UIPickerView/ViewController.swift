14dde99c00ac0293ea740d33737b9a4285445233
mohamedosama2074 <iosengks74@gmail.com>
2019-08-28 20:11:44 +0200

    UIPickerView

--- UIPickerView/ViewController.swift
+++ UIPickerView/ViewController.swift
@@ -10,11 +10,72 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textDateField: UITextField!
+    var selctedDay: String?
+    var data = ["Saturday" , "Sunday" , "Monday" , "TuesDay" , "thursday" , "wedensday" , "friday"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+      
+        makeDatePicker()
+       makeToolbar()
+        
     }
 
 
+}
+extension ViewController : UIPickerViewDelegate , UIPickerViewDataSource {
+  
+    
+    func makeDatePicker() {
+    
+        let datePicker = UIPickerView()
+        datePicker.delegate = self
+        datePicker.dataSource = self
+       
+        textDateField.inputView = datePicker
+        
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        
+        return data.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        
+        return data[row]
+    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
+        selctedDay = data[row]
+        
+        if let selected = selctedDay {
+        textDateField.text = selected
+        }
+        
+    }
+    
+    func makeToolbar(){
+        
+        let toolbar = UIToolbar()
+        toolbar.sizeToFit()
+        
+        let okButton = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(ViewController.doneButton))
+        toolbar.isUserInteractionEnabled = true
+        toolbar.setItems([okButton], animated: false)
+        textDateField.inputAccessoryView = toolbar
+    }
+    
+    @objc func doneButton(){
+        
+        view.endEditing(true)
+    }
+   
 }
 
